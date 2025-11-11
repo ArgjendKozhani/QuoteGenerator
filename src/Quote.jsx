6fc2adc6e5e4b90761a99b124962f8867ed9fb12
Argjend <argjendkozhani15@gmail.com>
2025-11-11 21:22:17 +0100

    Changed URL

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -7,7 +7,7 @@ function GetQuote() {
   
     async function showQuote() {
         try {
-            const url = "http://api.quotable.io/random"
+            const url = "https://api.quotable.io/random"
             const response = await fetch(url)
             const getData = await response.json()
             setData(getData)
